d856a65be2a972f806bf4d9d6a75ee830b07656a
lee7goal <909057156@qq.com>
2022-07-26 01:02:05 +0800

    添加了navie样式

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import App from './App.vue'
 import Router from './router'
 import store from './store'
 import './assets/main.css'
+import naive from 'naive-ui'
 
-createApp(App).use(Router).use(store).mount('#app')
+createApp(App).use(Router).use(store).use(naive).mount('#app')
