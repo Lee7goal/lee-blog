fff2400032b99b44e595c9ec66aaef0f0c835e60
liqigao <liqigao@libii.com>
2022-08-03 13:30:15 +0800

    css 样式修改

--- src/App.vue
+++ src/App.vue
@@ -1,107 +1,44 @@
 <template>
-  <n-menu v-model:value="activeKey" mode="horizontal" :options="menuOptions" />
-  <br>
-  <h1>Kiss Me More</h1>
+  <div class="container">
+    <header>
+      <img src="../public/avtar.jpg">
+      <h1>@lee7goal</h1>
+    </header>
+    
+    <ul>
+      <li>
+        <a href="#">
+          <img src="./assets/images/homepage.svg" alt="Homepage" width="20">
+          Offcial Website
+          </a>
+      </li>
+      <li>
+        <a href="https://www.youtube.com/channel/UCT8Wps4TxgZxgN_DVxDHodg">
+          <img src="./assets/images/youtube.svg" alt="Youtube" width="20">
+          YouTube
+          </a>
+      </li>
+      <li>
+        <a href="https://twitter.com/">
+          <img src="./assets/images/twitter.svg" alt="Twitter" width="20">
+          Twitter
+          </a>
+      </li>
+      <li>
+        <a href="https://github.com/">
+          <img src="./assets/images/github.svg" alt="Github" width="20">
+          Github
+          </a>
+      </li>
+    </ul>
+  </div>
+
 </template>
 
 <script>
-import { defineComponent, h, ref } from "vue";
-import { NIcon } from "naive-ui";
-import {
-  BookOutline as BookIcon,
-  PersonOutline as PersonIcon,
-  WineOutline as WineIcon
-} from "@vicons/ionicons5";
 
-function renderIcon(icon) {
-  return () => h(NIcon, null, { default: () => h(icon) });
-}
-
-const menuOptions = [
-  {
-    label: () => h("a", {
-      href: "https://baike.baidu.com/item/%E4%B8%94%E5%90%AC%E9%A3%8E%E5%90%9F",
-      target: "_blank",
-      rel: "noopenner noreferrer"
-    }, "且听风吟"),
-    key: "hear-the-wind-sing",
-    icon: renderIcon(BookIcon)
-  },
-  {
-    label: "1973年的弹珠玩具",
-    key: "pinball-1973",
-    icon: renderIcon(BookIcon),
-    disabled: true,
-    children: [
-      {
-        label: "鼠",
-        key: "rat"
-      }
-    ]
-  },
-  {
-    label: "寻羊冒险记",
-    key: "a-wild-sheep-chase",
-    icon: renderIcon(BookIcon),
-    disabled: true
-  },
-  {
-    label: "舞，舞，舞",
-    key: "dance-dance-dance",
-    icon: renderIcon(BookIcon),
-    children: [
-      {
-        type: "group",
-        label: "人物",
-        key: "people",
-        children: [
-          {
-            label: "叙事者",
-            key: "narrator",
-            icon: renderIcon(PersonIcon)
-          },
-          {
-            label: "羊男",
-            key: "sheep-man",
-            icon: renderIcon(PersonIcon)
-          }
-        ]
-      },
-      {
-        label: "饮品",
-        key: "beverage",
-        icon: renderIcon(WineIcon),
-        children: [
-          {
-            label: "威士忌",
-            key: "whisky"
-          }
-        ]
-      },
-      {
-        label: "食物",
-        key: "food",
-        children: [
-          {
-            label: "三明治",
-            key: "sandwich"
-          }
-        ]
-      },
-      {
-        label: "过去增多，未来减少",
-        key: "the-past-increases-the-future-recedes"
-      }
-    ]
-  }
-];
-
-export default defineComponent({
-  setup() {
-    return {
-      activeKey: ref(null),
-      menuOptions
-    };
-  }
-});
 </script>
+
+<style>
+
+</style>
